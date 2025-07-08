5e8f0bed79be300b11aa2126e4cea20b515ff8fa
Rushikesh Dhande <161755491+rushiikesh96@users.noreply.github.com>
2025-07-08 22:50:16 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import { Server } from "socket.io";
 const httpServer = createServer();
 const io = new Server(httpServer, {
   cors: {
-    origin: "https://tic-tac-toe-multiplayer-live.onrender.com", // ✅ FIXED: removed trailing slash
+    origin: "https://tictactoe-m81a.onrender.com/", // ✅ FIXED: removed trailing slash
     methods: ["GET", "POST"]
   }
 });
