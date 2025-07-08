f84f7dc1c6f3affd0a10a535a0de13a16f8904d0
Rushikesh Dhande <161755491+rushiikesh96@users.noreply.github.com>
2025-07-08 22:39:47 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -128,7 +128,7 @@ const App = () => {
     const username = result.value;
     setPlayerName(username);
 
-    const newSocket = io("https://tic-tac-toe-multiplayer-afye.onrender.com", {
+    const newSocket = io("https://tictactoe-bted.onrender.com", {
       autoConnect: true,
     });
 
